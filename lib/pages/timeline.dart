9f30293a40332a69ba4da4100bf36808fd4dfa6d
AMOREM <amorem@amorem.com>
2019-08-19 11:27:32 +0200

    HowTo : Fetching Collection and Document from Firestore

--- lib/pages/timeline.dart
+++ lib/pages/timeline.dart
@@ -1,7 +1,10 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 import '../widgets/header.dart';
 import '../widgets/progress.dart';
+
+final CollectionReference usersRef = Firestore.instance.collection('users');
 
 class Timeline extends StatefulWidget {
   @override
@@ -9,6 +12,26 @@ class Timeline extends StatefulWidget {
 }
 
 class _TimelineState extends State<Timeline> {
+  @override
+  void initState() {
+    getUserById();
+    super.initState();
+  }
+
+  getUsers() async {
+    QuerySnapshot snapshot = await usersRef.getDocuments();
+    snapshot.documents.forEach((DocumentSnapshot doc) {
+      print(doc.data);
+    });
+  }
+
+  getUserById() async {
+    final String id = 'cRsyLyqKIGm1F20cWINX';
+
+    DocumentSnapshot doc = await usersRef.document(id).get();
+    print('User $id ${doc.data}');
+  }
+
   @override
   Widget build(context) {
     return Scaffold(
