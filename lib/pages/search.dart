4d811c09831331a7cc6421e32cf0a4ffa766bf59
AMOREM <amorem@amorem.com>
2019-08-19 22:23:17 +0200

    Search Page, InputField + NoContent splash

--- lib/pages/search.dart
+++ lib/pages/search.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class Search extends StatefulWidget {
   @override
@@ -6,9 +7,52 @@ class Search extends StatefulWidget {
 }
 
 class _SearchState extends State<Search> {
+  AppBar buildSearchField() {
+    return AppBar(
+      backgroundColor: Colors.white,
+      title: TextFormField(
+        decoration: InputDecoration(
+          hintText: 'Search for a user...',
+          filled: true,
+          prefixIcon: Icon(Icons.account_box, size: 28.0),
+          suffixIcon: IconButton(
+            icon: Icon(Icons.clear),
+            onPressed: () {},
+          ),
+        ),
+      ),
+    );
+  }
+
+  Container buildNoContent() {
+    return Container(
+      child: Center(
+        child: ListView(
+          shrinkWrap: true,
+          children: <Widget>[
+            SvgPicture.asset('assets/images/search.svg', height: 300.0),
+            Text(
+              'Find Users',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                  color: Colors.white,
+                  fontStyle: FontStyle.italic,
+                  fontWeight: FontWeight.w600,
+                  fontSize: 60.0),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Text('Search');
+    return Scaffold(
+      backgroundColor: Theme.of(context).primaryColor.withOpacity(0.8),
+      appBar: buildSearchField(),
+      body: buildNoContent(),
+    );
   }
 }
 
