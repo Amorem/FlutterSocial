f8d3528c08fcdcd32940db80e8bf5ed4be04618a
AMOREM <amorem@amorem.com>
2019-08-20 16:22:52 +0200

    Get user location to fill location field

--- lib/pages/upload.dart
+++ lib/pages/upload.dart
@@ -4,6 +4,7 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:image/image.dart' as Im;
 import 'package:image_picker/image_picker.dart';
 import 'package:path_provider/path_provider.dart';
@@ -250,7 +251,7 @@ class _UploadState extends State<Upload> {
                 borderRadius: BorderRadius.circular(30),
               ),
               color: Colors.blue,
-              onPressed: () {},
+              onPressed: getUserLocation,
               icon: Icon(
                 Icons.my_location,
                 color: Colors.white,
@@ -260,6 +261,16 @@ class _UploadState extends State<Upload> {
         ],
       ),
     );
+  }
+
+  getUserLocation() async {
+    Position position = await Geolocator()
+        .getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+    List<Placemark> placemarks = await Geolocator()
+        .placemarkFromCoordinates(position.latitude, position.longitude);
+    Placemark placemark = placemarks[0];
+    String formatedAdress = "${placemark.locality}, ${placemark.country}";
+    locationControler.text = formatedAdress;
   }
 
   @override
