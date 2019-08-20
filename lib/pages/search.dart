acb3b10ce30d04aa008333b9fd15a6e2bad7bffb
AMOREM <amorem@amorem.com>
2019-08-20 08:15:36 +0200

    Orientation MediaQuery

--- lib/pages/search.dart
+++ lib/pages/search.dart
@@ -25,12 +25,14 @@ class _SearchState extends State<Search> {
   }
 
   Container buildNoContent() {
+    final Orientation orientation = MediaQuery.of(context).orientation;
     return Container(
       child: Center(
         child: ListView(
           shrinkWrap: true,
           children: <Widget>[
-            SvgPicture.asset('assets/images/search.svg', height: 300.0),
+            SvgPicture.asset('assets/images/search.svg',
+                height: orientation == Orientation.portrait ? 300.0 : 200.0),
             Text(
               'Find Users',
               textAlign: TextAlign.center,
