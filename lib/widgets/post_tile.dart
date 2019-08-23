a58117fde8fe00e7877de12c017bb4d13394967a
AMOREM <amorem@amorem.com>
2019-08-23 13:29:01 +0200

    Add PostDetails to Profile

--- lib/widgets/post_tile.dart
+++ lib/widgets/post_tile.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fluttershare/pages/post_screen.dart';
 import 'package:fluttershare/widgets/custom_image.dart';
 
 import '../models/post.dart';
@@ -6,10 +7,21 @@ import '../models/post.dart';
 class PostTile extends StatelessWidget {
   final Post post;
   PostTile(this.post);
+
+  showPost(context) {
+    Navigator.push(
+        context,
+        MaterialPageRoute(
+            builder: (context) => PostScreen(
+                  postId: this.post.postId,
+                  userId: this.post.ownerId,
+                )));
+  }
+
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: () {},
+      onTap: () => showPost(context),
       child: cachedNetworkImage(post.mediaUrl),
     );
   }
