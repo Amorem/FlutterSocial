6aff6ab665bca962bee7ff4bb183fa4b38541942
AMOREM <amorem@amorem.com>
2019-08-24 08:59:50 +0200

    onCreate, onUpdate, onDelete functions

--- functions/index.js
+++ functions/index.js
@@ -59,3 +59,90 @@ exports.onDeleteFollower = functions.firestore
       }
     });
   });
+
+exports.onCreatePost = functions.firestore
+  .document("/posts/{userId}/userPosts/{postId}")
+  .onCreate(async (snapshot, context) => {
+    const postCreated = snapshot.data();
+    const userId = context.params.userId;
+    const postId = context.params.postId;
+
+    // get all the followers from the user who made the post
+    const userFollowersRef = admin
+      .firestore()
+      .collection("followers")
+      .doc(userId)
+      .collection("userFollowers");
+    const querySnapshot = await userFollowersRef.get();
+
+    // add new post to each follower's timeline
+    querySnapshot.forEach(doc => {
+      const followerId = doc.id;
+      admin
+        .firestore()
+        .collection("timeline")
+        .doc(followerId)
+        .collection("timelinePosts")
+        .doc(postId)
+        .set(postCreated);
+    });
+  });
+
+exports.onUpdatePost = functions.firestore
+  .document("/posts/{userId}/userPosts/{postId}")
+  .onUpdate(async (change, context) => {
+    const postUdpated = change.after.data();
+    const userId = context.params.userId;
+    const postId = context.params.postId;
+
+    const userFollowersRef = admin
+      .firestore()
+      .collection("followers")
+      .doc(userId)
+      .collection("userFollowers");
+    const querySnapshot = await userFollowersRef.get();
+    querySnapshot.forEach(doc => {
+      const followerId = doc.id;
+      admin
+        .firestore()
+        .collection("timeline")
+        .doc(followerId)
+        .collection("timelinePosts")
+        .doc(postId)
+        .get()
+        .then(doc => {
+          if (doc.exists) {
+            doc.ref.update(postUdpated);
+          }
+        });
+    });
+  });
+
+exports.onDeletePost = functions.firestore
+  .document("/posts/{userId}/userPosts/{postId}")
+  .onDelete(async (snapshot, context) => {
+    const userId = context.params.userId;
+    const postId = context.params.postId;
+
+    const userFollowersRef = admin
+      .firestore()
+      .collection("followers")
+      .doc(userId)
+      .collection("userFollowers");
+    const querySnapshot = await userFollowersRef.get();
+    querySnapshot.forEach(doc => {
+      const followerId = doc.id;
+      admin
+        .firestore()
+        .collection("timeline")
+        .doc(followerId)
+        .collection("timelinePosts")
+        .doc(postId)
+        .get()
+        .then(doc => {
+          if (doc.exists) {
+            doc.ref.delete(postUdpated);
+          }
+        });
+    });
+  });
